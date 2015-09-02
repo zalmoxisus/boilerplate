cceefb96361dc3f3c0bafb1ea1c8af7c1d17370c
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-02 18:28:28 +0300

    Example with ES6 arrow functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
-  render() {
+  state = {name: this.props.name};
+
+  // custom function
+  sayHi = () => {
+    this.setState({name: 'Mike'});
+  };
+
+  render () {
     return (
-      <h1>{___('Hello, world')}!</h1>
+      <div>
+        <div><p>Hello, {this.state.name}! :)</p></div>
+        <div><input type="text" placeholder="Type a name here" onInput={(event) => this.setState({name:event.target.value})} /></div>
+        <div><button onClick={this.sayHi}>Click me</button></div>
+      </div>
     );
-  }
+  };
 }
