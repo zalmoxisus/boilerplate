24f4ef3570bc19246158719f6d4579d1ff9dfd4b
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-02 19:13:02 +0300

    Using ref Callback Attribute

--- src/App.js
+++ src/App.js
@@ -13,13 +13,9 @@ export default class App extends Component {
     return (
       <div>
         <div><p>Hello, {this.state.name}! :)</p></div>
-        <div><input ref="theInput" type="text" placeholder="Type a name here" onInput={(event) => this.setState({name:event.target.value})} /></div>
+        <div><input ref={(c) => {React.findDOMNode(c).focus(); this.refs = {'theInput': c };}} type="text" placeholder="Type a name here" onInput={(event) => this.setState({name:event.target.value})} /></div>
         <div><button onClick={this.sayHi}>Click me</button></div>
       </div>
     );
-  };
-
-  componentDidMount () {
-    React.findDOMNode(this.refs['theInput']).focus();
   };
 }
