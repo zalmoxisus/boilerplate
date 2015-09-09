05fa15c6f1f72ae20293198f4530536ea9f7b7f9
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-09 19:40:23 +0300

    Connecting dumb components to Redux

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './containers/App';
+import todoApp from './reducers';
 
-React.render(<App />, document.body);
+let store = createStore(todoApp);
+
+React.render(
+  // The child must be wrapped in a function
+  // to work around an issue in React 0.13.
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  document.body
+);
