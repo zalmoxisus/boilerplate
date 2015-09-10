f1bbe7a1aa2cac479c553938e99130b2613e6b63
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-10 15:33:32 +0300

    DevTools for Redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+// React components for Redux DevTools
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import App from './containers/App';
 import store from './store';
 
 
 React.render(
-  // The child must be wrapped in a function
-  // to work around an issue in React 0.13.
-  <Provider store={store}>
-    {() => <App />}
-  </Provider>,
+  <div>
+    {
+      // The child must be wrapped in a function
+      // to work around an issue in React 0.13.
+    }
+    <Provider store={store}>
+      {() => <App />}
+    </Provider>
+    <DebugPanel top right bottom>
+    <DevTools store={store} monitor={LogMonitor} />
+    </DebugPanel>
+  </div>,
   document.body
 );
