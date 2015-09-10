d921aa944fb7f377291dbce8857c2ced5ad7bd59
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-10 12:14:18 +0300

    Use redux-logger

--- src/store.js
+++ src/store.js
@@ -1,39 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
 import todoApp from './reducers';
 
-/**
- * Logs all actions and states after they are dispatched.
- */
-const logger = store => next => action => {
-  console.group(action.type);
-  console.info('dispatching', action);
-  let result = next(action);
-  console.log('next state', store.getState());
-  console.groupEnd(action.type);
-  return result;
-};
-
-/**
- * Sends crash reports as state is updated and listeners are notified.
- */
-const crashReporter = store => next => action => {
-  try {
-    return next(action);
-  } catch (err) {
-    console.error('Caught an exception!', err);
-    Raven.captureException(err, {
-      extra: {
-        action,
-        state: store.getState()
-      }
-    });
-    throw err;
-  }
-};
+const logger = createLogger({
+  level: 'info',
+  collapsed: true
+});
 
 let createStoreWithMiddleware = applyMiddleware(
-  logger,
-  crashReporter
+  logger
 )(createStore);
 
 export default createStoreWithMiddleware(todoApp);
