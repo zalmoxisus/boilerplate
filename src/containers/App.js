1d7c8e777f394607f710768474ce5329981dae03
Zalmoxisus <zalmoxisus@gmail.com>
2015-09-15 19:06:58 +0300

    Revert 4c1e318..ae91223
    This rolls back to commit 4c1e31802823688fc0ef52c7ea5a9109b3add696.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,33 @@
-import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import { addTodo, completeTodo, setVisibilityFilter } from '../actions';
+import React, { Component } from 'react';
 import AddTodo from '../components/AddTodo';
 import TodoList from '../components/TodoList';
 import Footer from '../components/Footer';
-import { visibleTodosSelector } from '../selectors/todoSelectors';
 
-class App extends Component {
+export default class App extends Component {
   render() {
-    // Injected by connect() call:
-    const { dispatch, visibleTodos, visibilityFilter } = this.props;
     return (
       <div>
         <AddTodo
           onAddClick={text =>
-            dispatch(addTodo(text))
+            console.log('add todo', text)
           } />
         <TodoList
-          todos={this.props.visibleTodos}
-          onTodoClick={index =>
-            dispatch(completeTodo(index))
+          todos={[{
+            text: 'Use Redux',
+            completed: true
+          }, {
+            text: 'Learn to connect it to React',
+            completed: false
+          }]}
+          onTodoClick={todo =>
+            console.log('todo clicked', todo)
           } />
         <Footer
-          filter={visibilityFilter}
-          onFilterChange={nextFilter =>
-            dispatch(setVisibilityFilter(nextFilter))
+          filter='SHOW_ALL'
+          onFilterChange={filter =>
+            console.log('filter change', filter)
           } />
       </div>
     );
   }
 }
-
-App.propTypes = {
-  visibleTodos: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
-  })),
-  visibilityFilter: PropTypes.oneOf([
-    'SHOW_ALL',
-    'SHOW_COMPLETED',
-    'SHOW_ACTIVE'
-  ]).isRequired
-};
-
-// Pass the selector to the connect component
-export default connect(visibleTodosSelector)(App);
