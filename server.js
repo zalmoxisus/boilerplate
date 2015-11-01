a904269435fa4501fe63ab8537fc1c6057e50c53
Zalmoxisus <zalmoxisus@gmail.com>
2015-11-01 18:08:36 +0200

    Configurable port

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.dev.config');
+var config = require('./webpack.config.development.js');
+
+var port = 3000;
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log('Listening at localhost:' + port);
 });
